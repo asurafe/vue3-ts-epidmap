48fe872ec1d1e0db1e4b7d7a5af37eeb75f1c700
LiuShiJie <onelana@163.com>
2022-11-03 14:13:11 +0800

    feat：前端后端初始化

---
+++ covid-19/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box">
+    <div class="box-left"></div>
+    <div class="box-center"></div>
+    <div class="box-right"></div>
+  </div>
+</template>
+
+<script setup>
+import {useCounterStore} from "./stores/counter.ts"
+const store = useCounterStore()
+store.getList()
+</script>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
+#app {
+  height: 100%;
+}
+.box {
+  height: 100%;
+  display: flex;
+  &-left {
+    width: 400px;
+    background: red;
+  }
+  &-center {
+    flex: 1;
+  }
+  &-right {
+    width: 400px;
+    background: green;
+  }
+}
+</style>
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,10 @@ import axios from "axios";
 const app: Express = express();
 const router: Router = express.Router();
 
+app.use("*",(req,res,next)=>{
+  res.header('Access-Control-Allow-Origin',"*")
+  next()
+})
 app.use("/api", router);
 
 router.get("/list", async (req: Request, res: Response) => {
