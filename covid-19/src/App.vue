a3580fa6ef90dcdd3a88dc92bc833a0d4c0fd34b
LiuShiJie <onelana@163.com>
2022-11-04 11:46:45 +0800

    feat:add echarts

--- covid-19/src/App.vue
+++ covid-19/src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="box">
+  <div :style="{ background: `url(${bg})` }" class="box">
     <div class="box-left"></div>
-    <div class="box-center"></div>
+    <div id="china" class="box-center"></div>
     <div class="box-right"></div>
   </div>
 </template>
 
-<script setup>
-import { useCounterStore } from "./stores/counter.ts";
+<script setup lang="ts">
+import bg from "./assets/ganyu.jpg";
+import { useCounterStore } from "./stores/counter";
+import { onMounted } from "vue";
+import * as echarts from "echarts";
+import { option } from "./utils/index";
+import "./assets/china.js";
 const store = useCounterStore();
 store.getList();
+onMounted(() => {
+  const charts = echarts.init(document.querySelector("#china") as HTMLElement);
+  charts.setOption(option);
+});
 </script>
 
 <style lang="less">
@@ -25,16 +34,15 @@ body,
 .box {
   height: 100%;
   display: flex;
+  background-size: 100vw !important;
   &-left {
-    width: 400px;
-    background: red;
+    width: 300px;
   }
   &-center {
     flex: 1;
   }
   &-right {
-    width: 400px;
-    background: green;
+    width: 300px;
   }
 }
 </style>
